cb2c0a172b3f34f97b73118a4d5a8057b3b974f0
rusito-23 <i.andruskiewitsch23@gmail.com>
2019-09-21 16:38:49 -0300

    REGFILE TestBench

--- source/regfile/regfile_tb.sv
+++ source/regfile/regfile_tb.sv
@@ -9,11 +9,42 @@ module regfile_tb();
 	// instantiate device under test
 	regfile dut(clk, we3, ra1, ra2, wa3, wd3, rd1, rd2);
 	
+	always begin
+		clk = 1; #10ns; clk = 0; #10ns;
+	end
+	
 	initial begin
-		ra1 = 0; #10ns;
-		ra1 = 2; #10ns;
-		ra2 = 2; #10ns;
-		ra2 = 3; #10ns;
+		
+		// test first 5 registers
+
+		
+		// write tests
+		we3 = 1;
+		wa3 = 23;
+		wd3 = 46; #5ns;
+		ra1 = 23; // check the value of register 23 is 46
+		#5ns;
+		
+		// no write tests
+		#10ns;
+		we3 = 0;
+		wa3 = 23;
+		wd3 = 96; #5ns;
+		ra1 = 23; // check the value of register 23 is still 46 (not 69)
+		#5ns;
+		we3 = 1; #10ns; // check the new value if we set write to true
+		
+		// write XZR
+		#10ns;
+		wa3 = 31;
+		wd3 = 23;
+		#5ns;
+		ra2 = 31;
+		#10ns;
+		
+		
+		$stop;
+		
 	end
 
 endmodule
