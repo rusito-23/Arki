5eda69c11a2fce92f37c8b291def4670653d46b4
rusito-23 <i.andruskiewitsch23@gmail.com>
2019-09-21 13:21:34 -0300

    Fix iMEM - iMEM TestBenches

--- source/imem/imem.sv
+++ source/imem/imem.sv
@@ -1,4 +1,35 @@
-module imem #(parameter N = 64)
-				(input logic [5:0] addr, output logic [N-1:0] q);
+module imem 
+			#(parameter N = 32)
+			(input logic [5:0] addr, 
+			 output logic [N-1:0] q);
+				
+	logic [N-1:0] ROM [0:18] = '{ 32'hf8000000,
+										  32'hf8008001,
+										  32'hf8010002,
+										  32'hf8018003,
+										  32'hf8020004,
+										  32'hf8028005,
+										  32'hf8030006,
+										  32'hf8400007,
+										  32'hf8408008,
+										  32'hf8410009,
+										  32'hf841800a,
+										  32'hf842000b,
+										  32'hf842800c,
+										  32'hf843000d,
+										  32'hcb0e01ce,
+										  32'hb400004e,
+										  32'hcb01000f,
+										  32'h8b01000f,
+										  32'hf803800f };
+										
+	always_comb begin
+		if (addr < 19) begin
+			q = ROM[addr];
+		end else begin
+			q = 0;
+		end
+	end
+	
 
 endmodule
---
+++ source/imem/imem_tb.sv
@@ -0,0 +1,35 @@
+module imem_tb();
+	logic [5:0] addr;
+	logic	[31:0] q; // internal variables
+	
+	// instantiate device under test
+	imem dut(addr, q);
+	
+	initial begin
+		addr = 0; #10ns;
+		addr = 1; #10ns;
+		addr = 2; #10ns;
+		addr = 3; #10ns;
+		addr = 4; #10ns;
+		addr = 5; #10ns;
+		addr = 6; #10ns;
+		addr = 7; #10ns;
+		addr = 8; #10ns;
+		addr = 9; #10ns;
+		addr = 10; #10ns;
+		addr = 11; #10ns;
+		addr = 12; #10ns;
+		addr = 13; #10ns;
+		addr = 14; #10ns;
+		addr = 15; #10ns;
+		addr = 16; #10ns;
+		addr = 17; #10ns;
+		addr = 18; #10ns;
+		addr = 19; #10ns;
+		addr = 20; #10ns;
+		addr = 21; #10ns;
+		addr = 22; #10ns;
+		addr = 23; #10ns;
+		addr = 24; #10ns;
+	end
+endmodule
