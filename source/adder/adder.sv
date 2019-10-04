da026985deb180260edd848202dfd7bad948b3ff
rusito-23 <i.andruskiewitsch23@gmail.com>
2019-10-04 11:39:39 -0300

    [FETCH] Multiplexor & Adder Implementation

---
+++ source/adder/adder.sv
@@ -0,0 +1,9 @@
+
+module adder
+			#(parameter N = 64)
+			(input logic [N-1: 0] s1, s2,
+			output logic [N-1: 0] res);
+			
+	assign res = s1 + s2;
+			
+endmodule
---
+++ source/fetch/fetch.sv
@@ -0,0 +1,10 @@
+
+module fetch(input logic PCSrc_F,
+				 input logic clk,
+				 input logic reset,
+				 input logic [63:0] PCBranch_F,
+				 output logic [63:0] imem_addr_F);
+				 
+	
+				 
+endmodule
