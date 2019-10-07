31f8641884e61959ff692e4354d90a50be5fa2a3
rusito-23 <i.andruskiewitsch23@gmail.com>
2019-10-07 10:41:10 -0300

    [SingleCycleProcessor] Adding Dynamic Parameter to Fetch & Decode to work better with given processor

--- source/stages/execute/execute.sv
+++ source/stages/execute/execute.sv
@@ -4,28 +4,29 @@
 * Runs the given instruction (after being decoded).
 */
 
-module execute(input logic AluSrc,
+module execute #(parameter N=64)
+					(input logic AluSrc,
 					input logic [3:0] AluControl,
-					input logic [63:0] PC_E,
-					input logic [63:0] signImm_E,
-					input logic [63:0] readData1_E,
-					input logic [63:0] readData2_E,
-					output logic [63:0] PCBranch_E,
-					output logic [63:0] aluResult_E,
-					output logic [63:0] writeData_E,
+					input logic [N-1:0] PC_E,
+					input logic [N-1:0] signImm_E,
+					input logic [N-1:0] readData1_E,
+					input logic [N-1:0] readData2_E,
+					output logic [N-1:0] PCBranch_E,
+					output logic [N-1:0] aluResult_E,
+					output logic [N-1:0] writeData_E,
 					output logic zero_E);
 					
 	// Out: PCBranch_E
-	logic [63:0] signImm_E_SL;
-	sl2 sl2EX(signImm_E, signImm_E_SL);
-	adder adderEX(signImm_E_SL, PC_E, PCBranch_E);
+	logic [N-1:0] signImm_E_SL;
+	sl2 #(N) sl2EX(signImm_E, signImm_E_SL);
+	adder #(N) adderEX(signImm_E_SL, PC_E, PCBranch_E);
 	
 	// Out: writeData_E
 	assign writeData_E = readData2_E;
 	
 	// ALU Multiplexor
-	logic [63:0] aluInput;
-	mux2 mux2EX(signImm_E, readData2_E, AluSrc, aluInput);
+	logic [N-1:0] aluInput;
+	mux2 #(N) mux2EX(signImm_E, readData2_E, AluSrc, aluInput);
 	
 	// Out: aluResult_E
 	alu aluEX(readData1_E, aluInput, AluControl, aluResult_E, zero_E);
--- source/stages/fetch/fetch.sv
+++ source/stages/fetch/fetch.sv
@@ -4,24 +4,25 @@
 * Returns the instruction to be executed next.
 */
 
-module fetch(input logic PCSrc_F,
+module fetch #(parameter N=64)
+				(input logic PCSrc_F,
 				 input logic clk,
 				 input logic reset,
-				 input logic [63:0] PCBranch_F,
-				 output logic [63:0] imem_addr_F);
+				 input logic [N-1:0] PCBranch_F,
+				 output logic [N-1:0] imem_addr_F);
 				 
 	// intialize variables
 	
-	logic [63:0] outMux = 64'b0;
-	logic [63:0] resultAdder = 64'b0;
-	logic [63:0] outFlipFlop = 64'b0;
-	logic [63:0] four = 64'd4;
+	logic [N-1:0] outMux = 'b0;
+	logic [N-1:0] resultAdder = 'b0;
+	logic [N-1:0] outFlipFlop = 'b0;
+	logic [N-1:0] four = 'd4;
 	
 	// modules
 	
-	mux2 muxFETCH(resultAdder, PCBranch_F, PCSrc_F, outMux);
-	flopr floprFETCH(clk, reset, outMux, outFlipFlop);
-	adder adderFETCH(four, outFlipFlop, resultAdder);
+	mux2 #(N) muxFETCH(resultAdder, PCBranch_F, PCSrc_F, outMux);
+	flopr #(N) floprFETCH(clk, reset, outMux, outFlipFlop);
+	adder #(N) adderFETCH(four, outFlipFlop, resultAdder);
 		
 	assign imem_addr_F = outFlipFlop;
 				 
