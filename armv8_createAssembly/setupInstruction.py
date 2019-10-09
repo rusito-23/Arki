54277447df8a7d072c659360de23e4d10e3804cd
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2019-10-09 15:57:59 -0300

    [ASSEMBLER] - Modify Makefile and Python Script to easy track the old tests -

--- armv8_createAssembly/setupInstruction.py
+++ armv8_createAssembly/setupInstruction.py
@@ -1,15 +1,16 @@
 # utf-8
 import re
+import argparse
 
-SOURCE_FILE='main.list'
+DEFAULT_SOURCE_FILE='main.list'
 DEST_FILE='../source/components/imem/imem.sv'
 
 HEXA_REGEX_PATTERN=':[\t][0-9a-fA-F]{8}'
 
-def main():
+def main(source_file):
 
     instructions = False
-    with open(SOURCE_FILE, 'r') as source_orig:
+    with open(source_file, 'r') as source_orig:
         source = " ".join([x.strip() for x in source_orig])
 
         # Find all instructions
@@ -18,11 +19,12 @@ def main():
         print("INSTRUCTIONS: {}".format(instructions))
         source_orig.close()
 
-    if (len(instructions) > 63):
-        print("ERROR - Max instructions len: 63")
-        exit(1)
-
     if (instructions):
+        if (len(instructions) > 63):
+            print("ERROR - Max instructions len: 63")
+            exit(1)
+
+        # Read original file
         dest_orig_read = open(DEST_FILE, 'r')
         dest = ''.join(dest_orig_read.readlines())
 
@@ -44,17 +46,22 @@ def main():
             instruction_set[i] = "32'h{}".format(instructions[i])
 
         # Re-Build the file
-
         instruction_set = ',\n\t\t\t\t\t\t\t\t\t\t\t'.join(instruction_set)
-
         new_file = first + '{' + instruction_set + '}' + third
-
         dest_orig = open(DEST_FILE, 'w')
         dest_orig.write(new_file)
-
         dest_orig_read.close()
         dest_orig.close()
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f',
+        type=str,
+        dest='source_file',
+        default=DEFAULT_SOURCE_FILE,
+        help='Source file for the instruction hexa list'
+    )
+    args = parser.parse_args()
+    main(args.source_file)
