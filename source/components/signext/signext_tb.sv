5e21903960f3f9424450e7910eb5b1734d1195e7
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2019-10-28 18:01:17 -0300

    [Exercise3] - ADDI Sign extendor -

--- source/components/signext/signext_tb.sv
+++ source/components/signext/signext_tb.sv
@@ -33,6 +33,13 @@ module signext_tb();
 		// Immediate -> -23
 		instruction = { 11'b11111000000, 9'b111101001, 12'b1}; #10ns;
 		
+		/* ADDI */
+		
+		// Immediate -> 23
+		instruction = { 10'b1001000100, 12'd23, 5'b1, 5'b1}; #10ns;
+		// Immediate -> -23
+		instruction = { 10'b1001000100, 12'b111111101001, 5'b1, 5'b1}; #10ns;
+		
 		/* NON-EXISTENT */
 		instruction = { 11'b10011000000, 9'd23, 12'b1}; #10ns;
 		
